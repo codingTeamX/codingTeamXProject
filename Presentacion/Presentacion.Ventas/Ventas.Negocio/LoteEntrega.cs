0653a868874a3413ecbd82c386627d16bf53a26d
daniel123sk8 <daniel_123_sk8@hotmail.com>
2017-02-28 09:32:49 -0400

    modificacion de lote de  entrega

--- Presentacion/Presentacion.Ventas/Ventas.Negocio/LoteEntrega.cs
+++ Presentacion/Presentacion.Ventas/Ventas.Negocio/LoteEntrega.cs
@@ -15,7 +15,103 @@ namespace Ventas.Negocio
         {
             get { return traducir(error); }
         }
-        
+                public DataTable datosLoteEntrega()
+        {
+            try
+            {
+                string sql = "select * from #tabla#";
+                sql = sql.Replace("#tabla#", tabla);
+                return traerDatos(sql);
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return null;
+            }
+        }
+        public string llave(OleDbTransaction tr)
+        {
+            try
+            {
+                string sql = "select max(id) from loteEntrega";
+                return traerDatos(sql, tr).Rows[0][0].ToString();
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return null;
+            }
+        }
+        public DataTable datosFechaLoteEntrega(string fecha)
+        {
+            try
+            {
+                string sql = "select * from #tabla# le inner join detalleLoteEntrega dle on dle.idLoteEntrega=le.id where fechaCompra like '" + fecha + "%'";
+                sql = sql.Replace("#tabla#", tabla);
+                return traerDatos(sql);
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return null;
+            }
+        }
+
+        public string[] vdatos = new string[5];
+
+        public int insertar(OleDbTransaction tr)
+        {
+            try
+            {
+                string sql = "insert into #tabla# values('#v1#','#v2#','#v3#','#v4#','#v5#')";
+                sql = sql.Replace("#tabla#", tabla);
+                sql = sql.Replace("#v1#", vdatos[0]);
+                sql = sql.Replace("#v2#", vdatos[1]);
+                sql = sql.Replace("#v3#", vdatos[2]);
+                sql = sql.Replace("#v4#", vdatos[3]);
+                sql = sql.Replace("#v5#", vdatos[4]);
+                return modificar(sql, tr);
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return 0;
+            }
+        }
+        public int actualizar(OleDbTransaction tr)
+        {
+            try
+            {
+                string sql = "update #tabla# set fechaCompra='#v1#',EstadoCompra='#v2#',idEmpleado='#v3#',idProveedor='#v4#' where id='#v0#'";
+                sql = sql.Replace("#tabla#", tabla);
+                sql = sql.Replace("#v0#", vdatos[0]);
+                sql = sql.Replace("#v1#", vdatos[1]);
+                sql = sql.Replace("#v2#", vdatos[2]);
+                sql = sql.Replace("#v3#", vdatos[3]);
+                sql = sql.Replace("#v4#", vdatos[4]);
+                return modificar(sql, tr);
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return 0;
+            }
+        }
+        public int eliminar(OleDbTransaction tr)
+        {
+            try
+            {
+                string sql = "delete from #tabla# where id='#v0#'";
+                sql = sql.Replace("#tabla#", tabla);
+                sql = sql.Replace("#v0#", vdatos[0]);
+                return modificar(sql, tr);
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return 0;
+            }
+        }
 
     }
 }

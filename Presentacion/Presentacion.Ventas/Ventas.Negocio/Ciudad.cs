4c21fe5db86ac8238c514606556b01921b15a093
Ivan1303 <ivan_isp@hotmail.com>
2017-02-28 21:19:02 -0400

    Modificacion de Ciudad Negocio

--- Presentacion/Presentacion.Ventas/Ventas.Negocio/Ciudad.cs
+++ Presentacion/Presentacion.Ventas/Ventas.Negocio/Ciudad.cs
@@ -15,7 +15,100 @@ namespace Ventas.Negocio
         {
             get { return traducir(error); }
         }
-        
+        public DataTable datosCiudad()
+        {
+            try
+            {
+                string sql = "select * from #tabla#";
+                sql = sql.Replace("#tabla#", tabla);
+                return traerDatos(sql);
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return null;
+            }
+        }
+        public string llave(OleDbTransaction tr)
+        {
+            try
+            {
+                string sql = "select max(id) from ciudad";
+                return traerDatos(sql, tr).Rows[0][0].ToString();
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return null;
+            }
+
+        }
+        public DataTable datosCiudadNombre(string nombre)
+        {
+            try
+            {
+                string sql = "select * from #tabla# where nombreCiudad like '" + nombre + "%'";
+                sql = sql.Replace("#tabla#", tabla);
+                return traerDatos(sql);
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return null;
+            }
+        }
+
+        public string[] vdatos = new string[3];
+
+        public int insertar(OleDbTransaction tr)
+        {
+            try
+            {
+                string sql = "insert into #tabla# values('#v1#','#v2#','#v3#')";
+                sql = sql.Replace("#tabla#", tabla);
+                sql = sql.Replace("#v1#", vdatos[0]);
+                sql = sql.Replace("#v2#", vdatos[1]);
+                sql = sql.Replace("#v3#", vdatos[2]);
+                return modificar(sql, tr);
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return 0;
+            }
+        }
+        public int actualizar(OleDbTransaction tr)
+        {
+            try
+            {
+                string sql = "update #tabla# set nombreCiudad='#v1#',idpais='#v2#' where id='#v0#'";
+                sql = sql.Replace("#tabla#", tabla);
+                sql = sql.Replace("#v0#", vdatos[0]);
+                sql = sql.Replace("#v1#", vdatos[1]);
+                sql = sql.Replace("#v2#", vdatos[2]);
+                return modificar(sql, tr);
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return 0;
+            }
+        }
+        public int eliminar(OleDbTransaction tr)
+        {
+            try
+            {
+                string sql = "delete from #tabla# where id='#v0#'";
+                sql = sql.Replace("#tabla#", tabla);
+                sql = sql.Replace("#v0#", vdatos[0]);
+                return modificar(sql, tr);
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return 0;
+            }
+        }
 
     }
 }

597b18b15e0336a9347cfb36ad90e25379f9e99e
Faviana Tapia Del Castillo <favianatapia@gmail.com>
2017-03-04 15:54:12 -0400

    se completó el formulario de cargos

--- Presentacion/Presentacion.Ventas/Presentacion.Ventas/FormCargo.cs
+++ Presentacion/Presentacion.Ventas/Presentacion.Ventas/FormCargo.cs
@@ -21,6 +21,206 @@ namespace Presentacion.Ventas
             InitializeComponent();
         }
 
-        
+        Cargo obcargo = new Cargo();
+        DataTable dtcargo = new DataTable();
+        CtrlTransaccion ctr = new CtrlTransaccion();
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (insetarDatos())
+            {
+                txt_buscar.Text = t1.Text;
+                if (cargarBusqueda())
+                    inicio();
+            }
+        }
+
+        private void btn_buscar_Click(object sender, EventArgs e)
+        {
+            if (cargarBusqueda())
+                inicio();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            if (actualizarDatos())
+            {
+                txt_buscar.Text = t1.Text;
+                if (cargarBusqueda())
+                    inicio();
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (eliminarDatos())
+            {
+                txt_buscar.Text = "";
+                if (cargarBusqueda())
+                    inicio();
+            }
+        }
+
+        private void btn_inicio_Click(object sender, EventArgs e)
+        {
+            if (hayDatos())
+                inicio();
+        }
+
+        private void btn_atras_Click(object sender, EventArgs e)
+        {
+            if (hayDatos() && puedeAtras())
+                atras();
+        }
+
+        private void btn_siguiente_Click(object sender, EventArgs e)
+        {
+            if (hayDatos() && puedeSiguiente())
+                siguiente();
+        }
+
+        private void btn_fin_Click(object sender, EventArgs e)
+        {
+            if (hayDatos())
+                fin();
+        }
+
+        bool cargarBusqueda()
+        {
+            dtcargo = obcargo.datosCargoNombre(txt_buscar.Text);
+            if (dtcargo != null)
+                dg1.DataSource = obcargo.datosCargoNombre(txt_buscar.Text);
+            else
+            {
+                MessageBox.Show(obcargo.ERROR);
+                return false;
+            }
+            return true;
+        }
+
+        int pos = -1;
+        string llave = "";
+
+        void cargarText()
+        {
+            if (hayDatos())
+            {
+                llave = dtcargo.Rows[pos][0].ToString();
+                t1.Text = dtcargo.Rows[pos][1].ToString();
+            }
+        }
+
+        void inicio()
+        {
+            pos = 0;
+            cargarText();
+        }
+
+        void fin()
+        {
+            pos = dtcargo.Rows.Count - 1;
+            cargarText();
+        }
+
+        void atras()
+        {
+            pos--;
+            cargarText();
+        }
+
+        void siguiente()
+        {
+            pos++;
+            cargarText();
+        }
+
+        bool insertarCargo(OleDbTransaction tr)
+        {
+            if (obcargo.llave(tr) == "")
+                obcargo.vdatos[0] = "1";
+            else
+                obcargo.vdatos[0] = (int.Parse(obcargo.llave(tr)) + 1).ToString();
+            obcargo.vdatos[1] = t1.Text;
+            if (obcargo.insertar(tr) == 0)
+            {
+                MessageBox.Show(obcargo.ERROR);
+                ctr.desTR(tr);
+                return false;
+            }
+            return true;
+        }
+
+        bool insetarDatos()
+        {
+            OleDbTransaction tr = ctr.iniciarTransaccion();
+            if (insertarCargo(tr))
+            {
+                ctr.finTR(tr);
+                return true;
+            }
+            return false;
+        }
+
+        bool actualizarCargo(OleDbTransaction tr)
+        {
+            obcargo.vdatos[0] = llave;
+            obcargo.vdatos[1] = t1.Text;
+            if (obcargo.actualizar(tr) == 0)
+            {
+                MessageBox.Show(obcargo.ERROR);
+                ctr.desTR(tr);
+                return false;
+            }
+            return true;
+        }
+
+        bool actualizarDatos()
+        {
+            OleDbTransaction tr = ctr.iniciarTransaccion();
+            if (actualizarCargo(tr))
+            {
+                ctr.finTR(tr);
+                return true;
+            }
+            return false;
+        }
+
+        bool eliminarCargo(OleDbTransaction tr)
+        {
+            obcargo.vdatos[0] = llave;
+            if (obcargo.eliminar(tr) == 0)
+            {
+                MessageBox.Show(obcargo.ERROR);
+                ctr.desTR(tr);
+                return false;
+            }
+            return true;
+        }
+
+        bool eliminarDatos()
+        {
+            OleDbTransaction tr = ctr.iniciarTransaccion();
+            if (eliminarCargo(tr))
+            {
+                ctr.finTR(tr);
+                return true;
+            }
+            return false;
+        }
+
+        bool hayDatos()
+        {
+            return dtcargo.Rows.Count != 0;
+        }
+
+        bool puedeSiguiente()
+        {
+            return pos < dtcargo.Rows.Count - 1;
+        }
+
+        bool puedeAtras()
+        {
+            return pos > 0;
+        }
     }
 }

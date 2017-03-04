6f5d3587e3e844a02c90aa1df0ede968fe21347b
Faviana Tapia Del Castillo <favianatapia@gmail.com>
2017-03-04 15:35:32 -0400

    Se completó la clase Empleado

--- Presentacion/Presentacion.Ventas/Ventas.Negocio/Empleado.cs
+++ Presentacion/Presentacion.Ventas/Ventas.Negocio/Empleado.cs
@@ -15,6 +15,100 @@ namespace Ventas.Negocio
         {
             get { return traducir(error); }
         }
-        
+
+        public DataTable datosEmpleado()
+        {
+            try
+            {
+                string sql = "select * from persona p inner join #tabla# c on c.id=p.id";
+                sql = sql.Replace("#tabla#", tabla);
+                return traerDatos(sql);
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return null;
+            }
+        }
+        public string llave(OleDbTransaction tr)
+        {
+            try
+            {
+                string sql = "select max(id) from Empleado";
+                return traerDatos(sql, tr).Rows[0][0].ToString();
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return null;
+            }
+
+        }
+        public DataTable datosEmpleadoNombre(string nombre)
+        {
+            try
+            {
+                string sql = "select * from persona p inner join #tabla# c on c.id=p.id where p.nombre like '" + nombre + "%'";
+                sql = sql.Replace("#tabla#", tabla);
+                return traerDatos(sql);
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return null;
+            }
+        }
+
+        public string[] vdatos = new string[2];
+
+        public int insertar(OleDbTransaction tr)
+        {
+            try
+            {
+                string sql = "insert into #tabla# values('#v1#','#v2#')";
+                sql = sql.Replace("#tabla#", tabla);
+                sql = sql.Replace("#v1#", vdatos[0]);
+                sql = sql.Replace("#v2#", vdatos[1]);
+                return modificar(sql, tr);
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return 0;
+            }
+        }
+        public int actualizar(OleDbTransaction tr)
+        {
+            try
+            {
+
+                string sql = "update #tabla# set descripcion='#v1#' where id='#v0#'";
+                sql = sql.Replace("#tabla#", tabla);
+                sql = sql.Replace("#v0#", vdatos[0]);
+                sql = sql.Replace("#v1#", vdatos[1]);
+                return modificar(sql, tr);
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return 0;
+            }
+        }
+        public int eliminar(OleDbTransaction tr)
+        {
+            try
+            {
+                string sql = "delete from #tabla# where id='#v0#'";
+                sql = sql.Replace("#tabla#", tabla);
+                sql = sql.Replace("#v0#", vdatos[0]);
+                return modificar(sql, tr);
+            }
+            catch (Exception e)
+            {
+                traducir(e.Message);
+                return 0;
+            }
+        }
+
     }
 }
